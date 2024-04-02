6cc09d7a5ce0707f92942fbf97e9282fac1508d5
syawalqi <galihmaulanasyawalqi@gmail.com>
2024-04-02 13:56:24 +0700

    menambahkan file 4 dan memasukan structur

---
+++ StructurC_4.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+#include <string>
+
+struct DetailAlamat {
+	string desa;
+	string kota;
+};
+
+struct mahasiswa {
+	string nim;
+	string nama;
+	DetailAlamat alamat;
+	int umur;
+};
