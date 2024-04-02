87071d25d9d2ab73727b4399535a6551b3485e1d
syawalqi <galihmaulanasyawalqi@gmail.com>
2024-04-02 13:35:28 +0700

    membuat int main dan mengisi struct

--- StructurC_1.cpp
+++ StructurC_1.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
 using namespace std;
 
-struct Mahasiswa {
+struct Mahasiswa
+{
 	string nim;
 	string nama;
 	string alamat;
 	int umur;
 };
+
+int main()
+{
+	Mahasiswa mhs;
+	cout << "Nomor Mahasiswa : ";
+	cin >> mhs.nim;
+	cout << "Nama Mahasiswa : ";
+	cin >> mhs.nama;
+	cout << "Alamat Mahasiswa : ";
+	cin >> mhs.alamat;
+	cout << "Umur Mahasiswa : ";
+	cin >> mhs.umur;
+}
