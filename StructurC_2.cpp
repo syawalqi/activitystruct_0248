bb6e2aeec534c4dc4ba96ecf290f65bc0045f2e7
syawalqi <galihmaulanasyawalqi@gmail.com>
2024-04-02 13:43:53 +0700

    menambahkan int main

--- StructurC_2.cpp
+++ StructurC_2.cpp
@@ -7,10 +7,28 @@ struct DetailAlamat {
 	string kota;
 };
 
-struct mahasiswa {
+struct ahasiswa {
 	string nim;
 	string nama;
 	DetailAlamat alamat;
 	int umur;
 };
 
+int main()
+{
+	mahasiswa mhs;
+	cout << "Nomor Mahasiswa : ";
+	cin >> mhs.nim;
+	cout << "Nama Mahasiswa : ";
+	cin >> mhs.nama;
+	cout << "Alamat Mahasiswa : ";
+	cin >> mhs.alamat;
+	cout << "Umur Mahasiswa : ";
+	cin >> mhs.umur;
+
+	cout << endl;
+	cout << "\n Nim : " << mhs.nim;
+	cout << "\n Nama : " << mhs.nama;
+	cout << "\n Alamat : " << mhs.alamat;
+	cout << "\n Umur : " << mhs.umur;
+}
