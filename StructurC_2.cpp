822aeea6ea5c21fbe472cb8a1140be4f0e86f406
syawalqi <galihmaulanasyawalqi@gmail.com>
2024-04-02 13:44:59 +0700

    memperbarui int main

--- StructurC_2.cpp
+++ StructurC_2.cpp
@@ -14,21 +14,19 @@ struct ahasiswa {
 	int umur;
 };
 
-int main()
-{
-	mahasiswa mhs;
-	cout << "Nomor Mahasiswa : ";
+
+int main() {
+	mahasiswa  mhs;
+	cout << "nomor Mahasiswa : ";
 	cin >> mhs.nim;
 	cout << "Nama Mahasiswa : ";
 	cin >> mhs.nama;
-	cout << "Alamat Mahasiswa : ";
-	cin >> mhs.alamat;
-	cout << "Umur Mahasiswa : ";
-	cin >> mhs.umur;
-
+    
 	cout << endl;
 	cout << "\n Nim : " << mhs.nim;
 	cout << "\n Nama : " << mhs.nama;
-	cout << "\n Alamat : " << mhs.alamat;
-	cout << "\n Umur : " << mhs.umur;
+	cout << "\n alamat : ";
+	cout << "\n Desa : " << mhs.alamat.desa;
+	cout << "\n Kota : " << mhs.alamat.kota;
+	cout << "\n Umur : " << mhs.umur;
 }
