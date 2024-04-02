000798ab4ad6c5dfa7e525e05f8595e5b71e6456
syawalqi <galihmaulanasyawalqi@gmail.com>
2024-04-02 13:54:32 +0700

    menambahkan isi main

--- StructurC_3.cpp
+++ StructurC_3.cpp
@@ -22,4 +22,13 @@ int main() {
 	cout << "Nama Mahasiswa : ";
 	getline(cin, mhs.nama);
 
+	cout << "alamat mahasiswa : " << endl;
+	cout << "\t nama Desa : ";
+	cin >> mhs.alamat.desa;
+	cout << "\t Nama kota : ";
+	cin >> mhs.alamat.kota;
+	cout << "Umur mahasiswa : ";
+	cin >> mhs.umur;
+
+
 }
