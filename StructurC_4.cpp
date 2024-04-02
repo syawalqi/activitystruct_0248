feb48d2ffd442dd1c53adc99913424732f4dbf02
syawalqi <galihmaulanasyawalqi@gmail.com>
2024-04-02 13:57:10 +0700

    membuat int main dan juga for

--- StructurC_4.cpp
+++ StructurC_4.cpp
@@ -13,3 +13,25 @@ struct mahasiswa {
 	DetailAlamat alamat;
 	int umur;
 };
+
+int main() {
+	mahasiswa mhs[3];
+	for (int i = 0; i < 3; i++) {
+		cout << "data ke-" << (i + 1) << ":" << endl;
+		cout << "Nomor mahasiswa : ";
+		getline(cin, mhs[i].nim);
+		cout << "Nama Mahasiswa : ";
+		getline(cin, mhs[i].nama);
+		
+		cout << "alamat mahasiswa : " << endl;
+		cout << "\t nama Desa : ";
+		cin >> mhs[i].alamat.desa;
+		cout << "\t Nama kota : ";
+		cin >> mhs[i].alamat.kota;
+		cout << "Umur mahasiswa : ";
+		cin >> mhs[i].umur;
+		cin.ignore(1, '\n');
+		cout << endl;
+	}
+
+}
