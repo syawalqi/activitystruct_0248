ab45df93fe70b19128c055f6c7ba842869cd9d38
syawalqi <galihmaulanasyawalqi@gmail.com>
2024-04-02 13:57:44 +0700

    menambahkan isi for

--- StructurC_4.cpp
+++ StructurC_4.cpp
@@ -34,4 +34,16 @@ int main() {
 		cout << endl;
 	}
 
+	for (int i = 0; i < 3; i++) {
+		cout << endl;
+		cout << "Data mahasiswa ke-" << (i + 1) << ":" << endl;
+		cout << "\n Nim :" << mhs[i].nim;
+		cout << "\n Nama : " << mhs[i].nama;
+		cout << "\n alamat : ";
+		cout << "\n \t Desa : " << mhs[i].alamat.desa;
+		cout << "\n \t Kota : " << mhs[i].alamat.kota;
+		cout << "\n Umur : " << mhs[i].umur;
+		cout << endl;
+		
+	}
 }
